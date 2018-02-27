55c6e03697b2f8f8fa2d1b63c3b17914255bceda
nawras <nawras.nasser@vistar.su>
2018-02-27 15:52:33 +0300

    remove cross

--- container/admin/src/main/java/com/rel/admin/config/security/SecurityConfig.java
+++ container/admin/src/main/java/com/rel/admin/config/security/SecurityConfig.java
@@ -1,44 +1,26 @@
 package com.rel.admin.config.security;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.servlet.ServletContextInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.AuthenticationEntryPoint;
-import org.springframework.security.web.access.channel.ChannelProcessingFilter;
-import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletResponse;
-import java.util.Arrays;
 
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-//    @Autowired
-//    private PasswordEncoder passwordEncoder;
-//    @Autowired
-//    AdminUserDetailsService adminUserDetailsService;
 
     private final static String LOGIN_URL ="api/auth/login";
 
@@ -61,27 +43,15 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
             "/login.html", "/","/*.css","/*.js","/*.png").permitAll()
             .anyRequest().authenticated()
             .and()
-            .cors()
-//            .configurationSource(corsConfigurationSource())
-            .and()
             .csrf().disable()
             .formLogin().loginPage("http://localhost:4200/login")
             .usernameParameter("username").passwordParameter("password")
             .loginProcessingUrl("/api/auth/login")
             .successForwardUrl("/")
-//            .loginProcessingUrl("http://localhost:4200/api/auth/login")
-            .and().logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout")).permitAll().logoutSuccessUrl("/");
+            .and().logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+            .permitAll().logoutSuccessUrl("/");
     }
 
-    @Bean
-    CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
-        configuration.setAllowedMethods(Arrays.asList("GET","POST","OPTIONS"));
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
-        return source;
-    }
 
     @Bean
     public ServletContextInitializer servletContextInitializer() {
@@ -99,19 +69,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Bean
     public AuthenticationProvider authenticationProvider() {
          return new AuthProvider();
-    }
-
-
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurerAdapter() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                .allowedOrigins("http://localhost:4200")
-                    .allowedMethods("POST", "DELETE","GET","OPTIONS");
-            }
-        };
     }
 
     @Bean
--- container/admin/src/main/java/com/rel/admin/config/security/SimpleCORSFilter.java
+++ container/admin/src/main/java/com/rel/admin/config/security/SimpleCORSFilter.java
@@ -15,46 +15,42 @@ import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
-@Component
-@Order(Ordered.HIGHEST_PRECEDENCE)
-public class SimpleCORSFilter implements Filter {
-
-    private final Logger log = LoggerFactory.getLogger(SimpleCORSFilter.class);
-
-    public SimpleCORSFilter() {
-        log.info("SimpleCORSFilter init");
-    }
-
-    @Override
-    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
-
-        HttpServletRequest request = (HttpServletRequest) req;
-        HttpServletResponse response = (HttpServletResponse) res;
-
-
-        response.setHeader("Access-Control-Allow-Origin", "http://localhost:4200");
-//        response.setHeader("Access-Control-Allow-Origin", "*");
-        response.setHeader("Access-Control-Allow-Credentials", "true");
-        response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
-        response.setHeader("Access-Control-Max-Age", "3600");
-        response.setHeader("Access-Control-Allow-Headers", "");
-
-
-        String hh =request.getMethod();
-
-        if (!request.getMethod().equals("OPTIONS")) {
-            chain.doFilter(req, res);
-        } else {
-            // do not continue with filter chain for options requests
-        }
-    }
-
-    @Override
-    public void init(FilterConfig filterConfig) {
-    }
-
-    @Override
-    public void destroy() {
-    }
-
-}
+//@Component
+//@Order(Ordered.HIGHEST_PRECEDENCE)
+//public class SimpleCORSFilter implements Filter {
+//
+//    private final Logger log = LoggerFactory.getLogger(SimpleCORSFilter.class);
+//
+//    public SimpleCORSFilter() {
+//        log.info("SimpleCORSFilter init");
+//    }
+//
+//    @Override
+//    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+//
+//        HttpServletRequest request = (HttpServletRequest) req;
+//        HttpServletResponse response = (HttpServletResponse) res;
+//
+//
+//        response.setHeader("Access-Control-Allow-Origin", "http://localhost:4200");
+//        response.setHeader("Access-Control-Allow-Credentials", "true");
+//        response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
+//        response.setHeader("Access-Control-Max-Age", "3600");
+//        response.setHeader("Access-Control-Allow-Headers", "");
+//
+//        if (!request.getMethod().equals("OPTIONS")) {
+//            chain.doFilter(req, res);
+//        } else {
+//            // do not continue with filter chain for options requests
+//        }
+//    }
+//
+//    @Override
+//    public void init(FilterConfig filterConfig) {
+//    }
+//
+//    @Override
+//    public void destroy() {
+//    }
+//
+//}
